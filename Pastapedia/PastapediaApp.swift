1985c60e987f64c645064fd64a7340a4c69b818a
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2020-12-29 10:48:48 -0800

    Initial Commit

---
+++ Pastapedia/PastapediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PastapediaApp.swift
+//  Pastapedia
+//
+//  Created by Nadia Barbosa on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct PastapediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
